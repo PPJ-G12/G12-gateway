0391f0f5ca69ad07f41e63210f0121a1e05fdc6c
Sebastián <sebastian.h.neira@outlook.com>
2024-11-18 10:00:39 -0300

    Se agrega nats server y Dockerfile

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,23 +1,10 @@
-import { Module } from '@nestjs/common';
-import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ORDER_SERVICE, envs } from 'src/config';
+import { Module } from "@nestjs/common";
+import { OrdersController } from "./orders.controller";
+import { NatsModule } from "../transports/nast.module";
 
 @Module({
   controllers: [OrdersController],
-  imports: [
-    ClientsModule.register([
-
-      {
-        name: ORDER_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: envs.ordersMicroserviceHost,
-          port: envs.ordersMicroservicePort
-        }
-      },
-
-    ]),
-  ]
+  imports: [NatsModule]
 })
-export class OrdersModule {}
+export class OrdersModule {
+}
