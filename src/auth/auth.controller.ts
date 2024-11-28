12f89c47b8c5406a560127b95345f4fb5ebcfbcf
Sebastián <sebastian.h.neira@outlook.com>
2024-11-28 15:57:18 -0300

    Se modifica el manejo de error en auth, (login y register)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, Inject, HttpException, HttpStatus } from '@nestjs/common';
-import { RegisterUserDto } from './dto/register-user.dto/register-user.dto';
-import { LoginUserDto } from './dto/login-user.dto/login-user.dto';
-import { ClientProxy } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config';
+import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { RegisterUserDto } from "./dto/register-user.dto/register-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto/login-user.dto";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
+import { NATS_SERVICE } from "src/config";
+import { catchError } from "rxjs";
 
 @Controller('auth')
 export class AuthController {
@@ -12,43 +13,19 @@ export class AuthController {
 
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
-    try {
-      return this.client.send('registerUser', registerUserDto);
-    } catch (error) {
-      this.handleError(error);
-    }
+    return this.client.send('registerUser', registerUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto) {
-    try {
-      return await this.client.send('loginUser', loginUserDto).toPromise();
-    } catch (error) {
-      this.handleError(error);
-    }
-  }
-
-  private handleError(error: any): never {
-    if (error?.response?.status) {
-      throw new HttpException(
-        error.response.message || 'Unexpected error',
-        error.response.status,
-      );
-    } else if (error.code === 'ECONNREFUSED') {
-      throw new HttpException(
-        'Service unavailable',
-        HttpStatus.SERVICE_UNAVAILABLE,
-      );
-    } else if (error.message?.includes('timeout')) {
-      throw new HttpException(
-        'Request timed out',
-        HttpStatus.GATEWAY_TIMEOUT,
-      );
-    } else {
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    return this.client.send('loginUser', loginUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 }
